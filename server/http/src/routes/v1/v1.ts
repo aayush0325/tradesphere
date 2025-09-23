a1642fa596e46104876ca4a18a76fe7d3e149fdc
Aayush Khanna <ayush25khanna@gmail.com>
2025-09-23 18:09:21 +0530

    Setup Prettier formatting and pre-commit hooks for backend (4 spaces)

--- server/http/src/routes/v1/v1.ts
+++ server/http/src/routes/v1/v1.ts
@@ -1,7 +1,7 @@
-import { Hono } from 'hono'
-import { clerkWebhooks } from './webhooks/clerk.js'
-import { users } from './users/users.js'
+import { Hono } from 'hono';
+import { clerkWebhooks } from './webhooks/clerk.js';
+import { users } from './users/users.js';
 
-export const v1 = new Hono()
-v1.route('/webhooks', clerkWebhooks)
-v1.route('/users', users)
+export const v1 = new Hono();
+v1.route('/webhooks', clerkWebhooks);
+v1.route('/users', users);
